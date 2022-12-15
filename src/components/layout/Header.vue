8c1d5cbb880db2e1e58b1b2101900d94c35ea024
Dmitrii Marov <marov@progressivemedia.ru>
2022-12-15 17:09:04 +0300

    create base project structure and implement first components

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <router-link to="/">UI Kit main page</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped lang="scss">
+.header {
+  background: #fff;
+  font-weight: bold;
+  padding: 0 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.07);
+  height: 62px;
+  display: flex;
+  align-items: center;
+  z-index: 1;
+  position: relative;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="heading-1">Welcome to UI-kit</h1>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
