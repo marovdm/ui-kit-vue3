8077937ccd4cd4c8626a3d9ccdb451ea5584b877
Dmitrii Marov <marov@progressivemedia.ru>
2022-12-18 16:10:29 +0300

    add table component

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import RadioButton from "@/views/RadioButton.vue";
 import ProgressBar from "@/views/ProgressBar.vue";
 import Input from "@/views/Input.vue";
 import Tabs from "@/views/Tabs.vue";
+import Table from "@/views/Table.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -50,6 +51,11 @@ const router = createRouter({
             path: '/tabs',
             name: 'Tabs',
             component: Tabs
+        },
+        {
+            path: '/table',
+            name: 'Table',
+            component: Table
         }
     ]
 })
